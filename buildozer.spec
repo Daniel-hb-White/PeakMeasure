6433ada9a1386ab7284931d36fef764d96024491
viktor.gauerhof <viktor.gauerhof@de.markant.com>
2025-03-10 11:48:32 +0100

    Squashed commit of the following:
    commit 5a5620fe347e5ecdceaf080cc4bc907b10da66d5
    Author: viktor.gauerhof <viktor.gauerhof@de.markant.com>
    Date:   Mon Mar 10 11:43:49 2025 +0100
    
        Add buildozer config file
    
    commit 93c6363b91d5672873ef686d38ba01d470e1ea9e
    Author: viktor.gauerhof <viktor.gauerhof@de.markant.com>
    Date:   Mon Mar 10 11:16:28 2025 +0100
    
        Implemented Camera
    
    commit 0f227bc1bd2406b13e550216f310fb3094ef6cf2
    Merge: a310e53 096ca96
    Author: viktor.gauerhof <viktor.gauerhof@de.markant.com>
    Date:   Mon Mar 10 09:43:03 2025 +0100
    
        Merge branch 'dev'
    
    commit 096ca96438bd09df75dee0a13676b9add2e49d15
    Author: Viktor Gauerhof <viktor.gauerhof@de.markant.com>
    Date:   Wed Feb 19 16:04:04 2025 +0100
    
        Added basic code structure & ui design

---
+++ buildozer.spec
@@ -0,0 +1,27 @@
+[app]
+
+title = PeakMeasure
+package.name = peakmeasure
+package.domain = gsog.de
+
+source.dir = .src
+source.include_exts = py,png,jpg,kv,atlas,mp3
+
+version = 0.1
+requirements = python3,kivy, https://github.com/kivymd/KivyMD/archive/master.zip,materialyoucolor,exceptiongroup,asyncgui,asynckivy
+
+orientation = portrait
+fullscreen = 0
+android.archs = arm64-v8a
+#p4a.branch = release-2022.12.20
+
+# iOS specific
+ios.kivy_ios_url = https://github.com/kivy/kivy-ios
+ios.kivy_ios_branch = main
+ios.ios_deploy_url = https://github.com/phonegap/ios-deploy
+ios.ios_deploy_branch = 1.12.2
+ios.codesign.allowed = false
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
---
+++ src/PeakMeasure.py
@@ -0,0 +1,52 @@
+import cv2
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.utils import platform
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.relativelayout import RelativeLayout
+
+from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
+
+
+class RootWidget(RelativeLayout):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.capture = cv2.VideoCapture(0)
+        self.image_widget = self.ids.camera_image
+        Clock.schedule_interval(self.update_camera, 1 / 30)
+
+    def update_camera(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.rotate(frame, cv2.ROTATE_180)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt="rgb")
+            texture.blit_buffer(frame.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+            self.image_widget.texture = texture
+
+    def on_stop(self):
+        if self.capture.isOpened():
+            self.capture.release()
+
+class PeakMeasureApp(MDApp):
+
+    # def on_start(self):
+    #     self.fps_monitor_start()
+
+    def build(self):
+        self.icon = './images/mokup_und_logo/PeakMeasureLogo.png'
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "White"
+
+        if platform not in ["android", "ios"]:
+            Window.size = (360, 640)
+
+        return RootWidget()
+    
+    def on_stop(self):
+        self.root.on_stop()
+
+
+PeakMeasureApp().run()
