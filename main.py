f74ac5c30328929f62a39f649f7f938d5b268244
viktor.gauerhof <viktor.gauerhof@de.markant.com>
2025-03-21 13:28:48 +0100

    Revert camera code

--- main.py
+++ main.py
@@ -3,44 +3,23 @@ from PIL import Image
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.utils import platform
-from kivy.uix.camera import Camera
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.camera import Camera
 from kivy.graphics.texture import Texture
+
 from kivy.clock import Clock
 
 if platform == "android":
-    from android.permissions import request_permissions, Permission, check_permission  # type: ignore
+    from android.permissions import request_permissions, Permission  # type: ignore
 
 
 class RootWidget(RelativeLayout):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # Schedule a check for camera permissions in the main thread
-        Clock.schedule_interval(self.check_camera_permissions, 1)
-
-    def check_camera_permissions(self, *args):
-        if platform == "android":
-            if check_permission(Permission.CAMERA):
-                print("Success: Camera permission already granted.")
-                self.setup_camera()
-                Clock.unschedule(self.check_camera_permissions)
-            else:
-                print("Error: Camera permission not granted yet.")
-        else:
-            # On non-Android platforms, simulate permission granted
-            self.setup_camera()
-            Clock.unschedule(self.check_camera_permissions)
-
     def setup_camera(self):
-        print("Setting up the camera...")
-        window_width, window_height = Window.size
-        # Hidden camera widget only for accessing the camera; not visible
-        self.camera = Camera(resolution=(int(window_width), int(window_height)), play=True, opacity=0)
-        self.add_widget(self.camera)
-
-        # Schedule the update_image method to process the camera feed
-        Clock.schedule_interval(self.update_image, 1.0 / 30.0)
+        self.camera = self.ids.hidden_camera
+        if self.camera:
+            self.camera.play = True
+            Clock.schedule_interval(self.update_image, 1.0 / 30.0)
 
     def update_image(self, dt):
         if self.camera and self.camera.texture:
@@ -50,12 +29,21 @@ class RootWidget(RelativeLayout):
             pixels = np.frombuffer(texture.pixels, dtype=np.uint8).reshape(height, width, 4)
             pil_image = Image.fromarray(pixels, mode='RGBA')
 
-            transformed_image = pil_image.rotate(90, expand=True)
+            transformed_image = pil_image.rotate(180, expand=True)
 
             rotated_texture = Texture.create(size=(transformed_image.width, transformed_image.height), colorfmt='rgba')
             rotated_texture.blit_buffer(transformed_image.tobytes(), colorfmt='rgba', bufferfmt='ubyte')
 
             self.ids.image_camera.texture = rotated_texture
+
+    def update_labe_distance_value(self, distance):
+        self.ids.label_distance.text = f"Distanz:\n{distance}m"
+
+    def update_label_height_value(self, height):
+        self.ids.label_height.text = f"Höhe:\n{height}m"
+
+    def text_field_person_height_on_text(self, text):
+        print(f"Text entered: {text}")
 
     def update_labe_distance_value(self, distance):
         self.ids.label_distance.text = f"Distanz:\n{distance}m"
@@ -74,12 +62,6 @@ class Main(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "White"
 
-        # Ask for permissions if on Android
-        if platform ==  "android":
-            self.request_app_permissions()
-        else:
-            Window.size = (360, 640)
-
         return RootWidget()
 
     def request_app_permissions(self):
@@ -88,8 +70,16 @@ class Main(MDApp):
     def on_app_permissions_result(self, permissions, results):
         if Permission.CAMERA in permissions and results[permissions.index(Permission.CAMERA)]:
             print("Success: Camera permission granted.")
+            self.root.setup_camera()
         else:
             print("Error: Camera permission not granted.")
+
+    def on_start(self):
+        if platform not in ["android", "ios"]:
+            Window.size = (360, 640)
+            self.root.setup_camera()
+        elif platform == "android":
+            self.request_app_permissions()
 
 
 Main().run()
